ce0892f653ca0346f2053640584da44f7b142665
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2025-11-18 14:51:35 -0800

    I blame frito for everything

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,30 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        int color = 0xc4246D;
+        int blueMask = 0x0000FF;
+
+        int blue = color & blueMask;
+
+        int greenMask = 0x00FF00;
+
+        int green = (color & greenMask) >> 8;
+
+        // System.out.println(Integer.toHexString(blue));
+        System.out.println(Integer.toHexString(green));
+
+        // int x = 0b10101;
+        // int p = 0b11001;
+
+        // int q = x & p;
+
+        // int y = 0xcf4e;
+        // int z = 94;
+
+        // System.out.println(Integer.toBinaryString(q));
+
+        // System.out.println(Integer.toBinaryString(z));
+        // System.out.println(Integer.toHexString(z));
+
+
     }
 }
